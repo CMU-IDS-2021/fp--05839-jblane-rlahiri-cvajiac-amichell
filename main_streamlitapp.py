f5d228b184130dacb09ec18ca0d1b62d510bc53f
Riddhiman Lahiri <78439141+Ridman1997@users.noreply.github.com>
2021-04-10 19:34:09 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -14,26 +14,73 @@ from datetime import date
 import collections
 import base64
 from vega_datasets import data
-source = data.movies.url
+import awesome_streamlit as ast
+import home
+import interface
+#import src.style
 st.set_page_config(layout="wide")
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+#st.set_page_config(layout="wide")
+ast.core.services.other.set_logging_format()
 
-local_css("style.css")
+PAGES = {
+    "Home": home,
+    "Main Interface": interface,
+    #"Interface": src.resources,
 
-st.title("⭐Towards a Better Spark UI⭐")
-st.text("An Interactive Visualization")
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://spark.apache.org/images/spark-logo-back.png");
-background-size: cover;
 }
-</style>
-'''
+def main():
+    """Main function of the App"""
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+    page = PAGES[selection]
+    
+    with st.spinner(f"Loading {selection} ..."):
+        ast.shared.components.write_page(page)
+    
+   
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This app is maintained by  Janice Blane, mailto:jblane@andrew.cmu.edu
+        Riddhiman Lahiri, (mailto:rlahiri@andrew.cmu.edu)
+        André Michell, (mailto:amichell@andrew.cmu.edu)
+        Catalina Vajiac, (mailto:cvajiac@andrew.cmu.edu)
+        """
+    )
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+#source = data.movies.url
+#st.set_page_config(layout="wide")
+#def local_css(file_name):
+#    with open(file_name) as f:
+#        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#local_css("style.css")
+
+#st.title("⭐Towards a Better Spark UI⭐")
+#st.text("An Interactive Visualization")
+#page_bg_img = '''
+#<style>
+#body {
+#background-image: url("https://spark.apache.org/images/spark-logo-back.png");
+#background-size: cover;
+#}
+#</style>
+#'''
+
+#st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 
@@ -73,78 +120,83 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 #    )
 
 #st.set_page_config(layout="wide")
-st.markdown("""
- * Use the menu at left to select data and set plot parameters
- * Your plots will appear below
-""")
 
 
-st.sidebar.markdown("## Select Optimization")
+
+#st.markdown("""
+# * Use the menu at left to select data and set plot parameters
+# * Your plots will appear below
+#""")
+
+
+#st.sidebar.markdown("## Select Optimization")
 #set_png_as_page_bg('apache-spark1.jpg')
 
 #st.sidebar.markdown('#### Q-tranform plot')
 #vmax = st.sidebar.slider('Colorbar Max Energy', 10, 500, 25)  # min, max, default
 #qcenter = st.sidebar.slider('Data', 5, 120, 5)  # min, max, default
 #qrange = (int(qcenter*0.8), int(qcenter*1.2))
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/jpg;base64,%s");
-    background-size: cover;
-    opacity: 1;
-    }
-    </style>
-    ''' % bin_str
+
+
+#@st.cache(allow_output_mutation=True)
+#def get_base64_of_bin_file(bin_file):
+#    with open(bin_file, 'rb') as f:
+#        data = f.read()
+#    return base64.b64encode(data).decode()
+#def set_png_as_page_bg(png_file):
+#    bin_str = get_base64_of_bin_file(png_file)
+#    page_bg_img = '''
+#    <style>
+#    body {
+#    background-image: url("data:image/jpg;base64,%s");
+#    background-size: cover;
+#    opacity: 1;
+#    }
+#    </style>
+#    ''' % bin_str
     
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-set_png_as_page_bg('apache-spark1.jpg')
-task = st.selectbox(
-        "Which problem do you want to Visualize?", ["Word Count","Spark for ML","Apply Optimizations to an Extract, Transform, Load (ETL) job"])
-plot=alt.Chart(source).mark_bar().encode(
-    alt.X("IMDB_Rating:Q"),
-    y='count()',
+#    st.markdown(page_bg_img, unsafe_allow_html=True)
+#    return
+#set_png_as_page_bg('apache-spark1.jpg')
+#task = st.selectbox(
+#        "Which problem do you want to Visualize?", ["Word Count","Spark for ML","Apply Optimizations to an Extract, Transform, Load (ETL) job"])
+#plot=alt.Chart(source).mark_bar().encode(
+#    alt.X("IMDB_Rating:Q"),
+#    y='count()',
     
-).properties(width=400,
-    height=200)
-data_spill_button=st.sidebar.radio("Show data spill to memory and disk",("Yes","No"))
+#).properties(width=400,
+#    height=200)
+#data_spill_button=st.sidebar.radio("Show data spill to memory and disk",("Yes","No"))
 
-suffle_read_button=st.sidebar.radio("Show shuffle read and write quantities",("Yes","No"))
+#suffle_read_button=st.sidebar.radio("Show shuffle read and write quantities",("Yes","No"))
 
-Dataio_button=st.sidebar.radio("Show data input and output",("Yes","No"))
+#Dataio_button=st.sidebar.radio("Show data input and output",("Yes","No"))
 
-if st.checkbox("Optimization 1"):
+#if st.checkbox("Optimization 1"):
     #TODO
-    st.write("Implement Optimization 1 on Data Set")
+#    st.write("Implement Optimization 1 on Data Set")
     
-if st.checkbox("Optimization 2"):
+#if st.checkbox("Optimization 2"):
     #TODO
-    st.write("Implement Optimization 2 on Data Set")
+#    st.write("Implement Optimization 2 on Data Set")
     
     
-if st.checkbox("Optimization 3"):
+#if st.checkbox("Optimization 3"):
     #TODO 
-    st.write("Implement Optimization 3 on Data Set")
-col1, col2, col3 = st.beta_columns(3)
-if data_spill_button=="Yes":
-    col1.header("Data Spill to Memory and Disk")
-    col1.write(plot)
+#    st.write("Implement Optimization 3 on Data Set")
+#col1, col2, col3 = st.beta_columns(3)
+#if data_spill_button=="Yes":
+#    col1.header("Data Spill to Memory and Disk")
+#    col1.write(plot)
     #row2_1, row2_2, row2_3 = st.beta_columns((2,1,1))
 
-if suffle_read_button=="Yes":
-    col2.header("Shuffle Read and Write")
-    col2.write(plot)
-if Dataio_button=="Yes":
-    col3.header("Data input and output")
-    col3.write(plot)
-st.write("** Display Data after Optimizations**")
-optimized=st.radio("",("Yes","No"))
-if optimized=="Yes":
-    st.write(plot.properties(width=600,height=300))
+#if suffle_read_button=="Yes":
+#    col2.header("Shuffle Read and Write")
+#    col2.write(plot)
+#if Dataio_button=="Yes":
+#    col3.header("Data input and output")
+#    col3.write(plot)
+#st.write("** Display Data after Optimizations**")
+#optimized=st.radio("",("Yes","No"))
+#if optimized=="Yes":
+#    st.write(plot.properties(width=600,height=300))
