18594bd6768840d3e4b8c7fb8fba604b77aac1de
Riddhiman Lahiri <78439141+Ridman1997@users.noreply.github.com>
2021-04-10 21:10:29 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -28,13 +28,227 @@ st.set_page_config(layout="wide")
     #"Interface": src.resources,
 
 #}
+class home1:
+    from datetime import date
+
+#def local_css(file_name):
+#    with open(file_name) as f:
+#        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+#local_css("style1.css")
+
+    
+
+#logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+
+
+    def write(self):
+	    page_bg_img = '''
+	    <style>
+	    body 	{
+    		    color: #fff;
+    		    background-color: #4F8BF9;
+		    }
+
+	    .stButton>button {
+		    color: #4F8BF9;
+		    border-radius: 50%;
+    		    height: 3em;
+    		    width: 3em;
+		    }
+
+	    .stTextInput>div>div>input {
+   		     color: #4F8BF9;
+		    }
+	    </style>
+	    '''
+	    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+	    st.title("⭐Why Do We Need a Better Spark UI?⭐")
+	    #st.text("An Interactive Visualization")#
+	    st.markdown("---")
+	    st.markdown("_**First, WHAT IS SPARK?!**_" )
+	    components.html(
+    	    """
+    	    <!DOCTYPE html>
+    	    <html>
+    	    <head>
+    	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    	    <style>
+    	    .container {
+     	    position: relative;
+      	    width: 30%;
+    	    }
+    
+    	    .image {
+      	    display: block;
+      	    width: 100%;
+      	    height: auto;
+    	    }
+    	    </style>
+    	    </head>
+    	    <body>
+    
+    	    <!--<h2>Current Results View</h2>-->
+    	    <!--<p>Hover over the image to see the effect.</p>-->
+    
+    	    <div class="container">
+      	    <img src="https://spark.apache.org/images/spark-logo-trademark.png" alt="Current Spark Results" class="image" >
+    	    </div>
+    
+    	    </body>
+    	    </html>
+    	    """,
+    	    height=200
+    	    )
+	    st.markdown("""Originated at UC Berkeley in 2009, Apache Spark is a unified analytics 
+                engine for big data processing. It includes support for SQL queries, streaming 
+                data, machine learning, and graph processing. Spark runs on Hadoop, Apache Mesos, 
+                Kubernetes, standalone, or in the cloud. Internet companies such as 
+                Netflix, Yahoo, and eBay have deployed Spark at massive scale, collectively 
+                processing multiple petabytes of data on clusters of over 8,000 nodes.""")
+
+
+	    st.markdown("---")
+	    st.markdown("_**So What's the Problem?**_" )
+	    st.markdown("_Here's what Spark results look like now:_" )
+	    components.html(
+    	    """
+    	    <!DOCTYPE html>
+    	    <html>
+    	    <head>
+    	    <meta name="viewport" content="width=device-width, initial-scale=1">
+    	    <style>
+    	    .container {
+      	    position: relative;
+      	    width: 50%;
+    	    }
+    
+    	    .image {
+      	    display: block;
+      	    width: 200%;
+      	    height: auto;
+    	    }
+    
+    	    .overlay {
+      	    position: absolute;
+      	    top: 0;
+      	    bottom: 0;
+      	    left: 0;
+      	    right: 0;
+      	    height: 100%;
+      	    width: 200%;
+      	    opacity: 0;
+      	    transition: .5s ease;
+      	    background-color: #C88F0A;
+    	    }
+    
+    	    .container:hover .overlay {
+      	    opacity: 1;
+    	    }
+    
+    	    .text {
+      	    color: white;
+      	    font-size: 30px;
+      	    position: absolute;
+      	    top: 50%;
+      	    left: 50%;
+      	    -webkit-transform: translate(-50%, -50%);
+      	    -ms-transform: translate(-50%, -50%);
+      	    transform: translate(-50%, -50%);
+      	    text-align: center;
+    	    }
+    	    </style>
+    	    </head>
+    	    <body>
+    
+    	    <!--<h2>Current Results View</h2>-->
+    	    <!--<p>Hover over the image to see the effect.</p>-->
+    
+    	    <div class="container">
+      	    <img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/job_example.png?raw=true" alt="Current Spark Results" class="image" >
+      	    <div class="overlay">
+            <div class="text">Some useful stuff, but how to make sense of it?!</div>
+      	    </div>
+    	    </div>
+    
+    	    </body>
+    	    </html>
+    	    """,
+    	    height=300
+    	    )
+	    st.markdown("""This UI displays some information, such as the job duration, stages, and a progress bar.
+                However, all the information is displayed in tabular format and we don’t see a lot of other useful information,
+                such as memory usage or shuffle read/writes. While this information is available, it is not presented with
+                effective visual encodings that identify clearly the implication of different characteristics of Spark jobs.
+                Furthermore, once the end user has hundreds or thousands of jobs, these tables become impossible to parse.""")
 
 
 
 
 
+class interface:
+    
+    def write(self):
+        source = data.movies.url
+	    #st.set_page_config(layout="wide")
+        st.title("A Better Spark User Interface")
+        st.markdown("""
+ 	    * Use the menu at left to select data and set plot parameters
+ 	    * Your plots will appear below
+	    """)
 
+        st.sidebar.markdown("## Select Optimization")
+	    #set_png_as_page_bg('apache-spark1.jpg')
 
+	    #st.sidebar.markdown('#### Q-tranform plot')
+	    #vmax = st.sidebar.slider('Colorbar Max Energy', 10, 500, 25)  # min, max, default
+	    #qcenter = st.sidebar.slider('Data', 5, 120, 5)  # min, max, default
+	    #qrange = (int(qcenter*0.8), int(qcenter*1.2))
+        task = st.selectbox(
+        	    "Which problem do you want to Visualize?", ["Word Count","Spark for ML","Apply Optimizations to an Extract, Transform, Load (ETL) job"])
+        plot=alt.Chart(source).mark_bar().encode(
+    	    alt.X("IMDB_Rating:Q"),
+    	    y='count()',
+    
+	    ).properties(width=400,
+    	    height=200)
+        data_spill_button=st.sidebar.radio("Show data spill to memory and disk",("Yes","No"))
+
+        suffle_read_button=st.sidebar.radio("Show shuffle read and write quantities",("Yes","No"))
+        Dataio_button=st.sidebar.radio("Show data input and output",("Yes","No"))
+	    #row2_1, row2_2, row2_3, row2_4 = st.beta_columns((2,1,1,1))
+        st.write("** Please select a checkbox to implement one or more optimization**")
+        if st.checkbox("Optimization 1"):
+    		    #TODO
+                st.write("Implement Optimization 1 on Data Set")
+    
+        if st.checkbox("Optimization 2"):
+    		    #TODO
+                st.write("Implement Optimization 2 on Data Set")
+    
+        if st.checkbox("Optimization 3"):
+    		    #TODO 
+                st.write("Implement Optimization 3 on Data Set")
+      
+        col1, col2, col3 = st.beta_columns(3)
+        if data_spill_button=="Yes":
+                col1.header("Data Spill to Memory and Disk")
+                col1.write(plot)
+    		    #row2_1, row2_2, row2_3 = st.beta_columns((2,1,1))
+
+        if suffle_read_button=="Yes":
+                col2.header("Shuffle Read and Write")
+                col2.write(plot)
+        if Dataio_button=="Yes":
+                col3.header("Data input and output")
+                col3.write(plot)
+        st.write("** Display Data after Optimizations**")
+        optimized=st.radio("",("Yes","No"))
+        if optimized=="Yes":
+                st.write(plot.properties(width=600,height=300))
 
 
 
