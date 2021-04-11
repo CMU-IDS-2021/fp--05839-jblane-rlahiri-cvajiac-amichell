48ab89ffa2e92efb592deace3091bc27006a7795
Riddhiman Lahiri <78439141+Ridman1997@users.noreply.github.com>
2021-04-10 21:31:25 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -30,10 +30,10 @@ st.set_page_config(layout="wide")
 #}
 
 def local_css(file_name):
-    with open(./file_name) as f:
+    with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("./style.css")
 class home1:
     from datetime import date
 
