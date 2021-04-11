d06eb29bd968be110e01b169fc927c1dc3321cf9
Riddhiman Lahiri <78439141+Ridman1997@users.noreply.github.com>
2021-04-10 21:28:45 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -28,6 +28,8 @@ st.set_page_config(layout="wide")
     #"Interface": src.resources,
 
 #}
+with open('./style.css') as f:
+	st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 class home1:
     from datetime import date
 
