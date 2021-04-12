94d8c7e505caef1514f9f2afb10dc313d05439c4
jtblane <63323583+jtblane@users.noreply.github.com>
2021-04-12 13:27:39 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -225,7 +225,7 @@ class home1:
     	    </body>
     	    </html>
     	    """,
-    	    height=500
+    	    height=600
     	    )
 	    st.markdown("""This UI displays some information, such as the job duration, stages, and a progress bar.
                 However, all the information is displayed in tabular format and we don’t see a lot of other useful information,
