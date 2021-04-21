d96fdefe943ee682b1da0500eb471fa359d660be
jtblane <63323583+jtblane@users.noreply.github.com>
2021-04-21 19:51:00 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -97,7 +97,7 @@ class home1:
 			<body>
 
 			<div class="container">
-	  		<img src="https://spark.apache.org/images/spark-logo-trademark.png" alt="Current Spark Results" class="image" style="height:200">
+	  		<img src="https://spark.apache.org/images/spark-logo-trademark.png" alt="Current Spark Results" class="image" style="height:100">
 			</div>
             
 			<p style="font-size:18px;font-family:verdana">Originated at UC Berkeley in 2009, Apache Spark is a unified analytics 
@@ -115,7 +115,7 @@ class home1:
 
 			</html>
 			""",
-			height=1000
+			height=600
 			)
 		
         st.markdown("---")
