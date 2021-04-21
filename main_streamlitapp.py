ca526982fcd9a1cdc6e10036681b41de151fc288
jtblane <63323583+jtblane@users.noreply.github.com>
2021-04-21 16:52:35 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -165,19 +165,19 @@ class home1:
               <!-- Full-width images with number and caption text -->
               <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
-                <img src="intro001.png" style="width:100%">
+                <img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/intro001.png?raw=true" style="width:100%">
                 <div class="text">Caption Text</div>
               </div>
             
               <div class="mySlides fade">
                 <div class="numbertext">2 / 3</div>
-                <img src="intro002.png" style="width:100%">
+                <img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/intro002.png?raw=true" style="width:100%">
                 <div class="text">Caption Two</div>
               </div>
             
               <div class="mySlides fade">
                 <div class="numbertext">3 / 3</div>
-                <img src="intro003.png" style="width:100%">
+                <img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/intro003.png?raw=true" style="width:100%">
                 <div class="text">Caption Three</div>
               </div>
             
