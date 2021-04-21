080d38a120b10459b1e38be02fc9cdcb2a9f985f
jtblane <63323583+jtblane@users.noreply.github.com>
2021-04-21 16:59:05 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -255,19 +255,19 @@ class home1:
               <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
                 <img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/intro001.png?raw=true" style="width:100%">
-                <div class="text">Caption Text</div>
+                <!--<div class="text">Caption Text</div>-->
               </div>
             
               <div class="mySlides fade">
                 <div class="numbertext">2 / 3</div>
                 <img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/intro002.png?raw=true" style="width:100%">
-                <div class="text">Caption Two</div>
+                <!--<div class="text">Caption Two</div>-->
               </div>
             
               <div class="mySlides fade">
                 <div class="numbertext">3 / 3</div>
                 <img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/intro003.png?raw=true" style="width:100%">
-                <div class="text">Caption Three</div>
+                <!--<div class="text">Caption Three</div>-->
               </div>
             
               <!-- Next and previous buttons -->
