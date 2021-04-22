ce216d7d8de531eb4ec4ad07328a1420e420be62
jtblane <63323583+jtblane@users.noreply.github.com>
2021-04-21 20:57:07 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -136,7 +136,7 @@ class home1:
 			<body>
 
 			<div class="container">
-	  		<img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/sparkCircle.png?raw=true" alt="SparkCircle" class="image" style="width:100%">
+	  		<img src="https://github.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/blob/main/sparkCircle.png?raw=true" alt="SparkCircle" class="image" style="height:f100%">
 			
             </div>
 			
@@ -144,7 +144,7 @@ class home1:
 
 			</html>
 			""",
-			height=600
+			height=400
 			)
         st.markdown("---")
         st.markdown("***HOW DOES IT WORK?***" )
