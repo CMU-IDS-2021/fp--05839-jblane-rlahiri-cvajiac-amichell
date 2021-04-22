00b5547578b7fb01d5dacb677524a5f23e071516
jtblane <63323583+jtblane@users.noreply.github.com>
2021-04-21 20:14:11 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -100,7 +100,7 @@ class home1:
 	  		<img src="https://spark.apache.org/images/spark-logo-trademark.png" alt="Current Spark Results" class="image" style="height:25">
 			</div>
             
-			<p style="font-size:18px;font-family:verdana">Originated at UC Berkeley in 2009, Apache Spark is a unified analytics 
+			<p class="listFont">Originated at UC Berkeley in 2009, Apache Spark is a unified analytics 
 			engine for big data processing. It includes support for SQL queries, streaming 
 			data, machine learning, and graph processing. Spark runs on Hadoop, Apache Mesos, 
 			Kubernetes, standalone, or in the cloud. Internet companies such as 
