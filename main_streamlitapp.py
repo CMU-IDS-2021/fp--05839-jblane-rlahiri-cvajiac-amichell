bce7b07db9428ab37fd6c2753ca4259966704444
jtblane <63323583+jtblane@users.noreply.github.com>
2021-04-21 20:33:53 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -97,7 +97,7 @@ class home1:
 			<body>
 
 			<div class="container">
-	  		<img src="https://spark.apache.org/images/spark-logo-trademark.png" alt="Current Spark Results" class="image" style="height:100%">
+	  		<img src="https://spark.apache.org/images/spark-logo-trademark.png" alt="sparklogo" class="image" style="width:100%">
 			</div>
 			
 			</body>
