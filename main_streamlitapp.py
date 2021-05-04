925de9e042a745214bf26ed066905e759d9cd1d4
swiminspace <andre.j.michell2@gmail.com>
2021-05-04 10:49:47 -0400

    Debug url string

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -356,6 +356,7 @@ class Interface:
         task_s = "wordcount" if task == "Word Count" else "etl"
         url = 'https://raw.githubusercontent.com/CMU-IDS-2021/fp--05839-jblane-rlahiri-cvajiac-amichell/' \
                'main/data/{}/{}-sanitized.json'.format(task_s, filename)
+        st.write(url)
 
         with st.beta_expander('Click Here to Display the Code!!!'):
             with open('spark/{}.py'.format(filename), 'r') as f:
